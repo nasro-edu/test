eddfc5e01ddb74d5130d5716dd5cc3622ee772c1
acadev CEO <nasro.edu@gmail.com>
2016-09-19 06:45:59 -0400

    added comments on what topics are covered

--- test_lectures/src/test_lectures/GeometricFigure.java
+++ test_lectures/src/test_lectures/GeometricFigure.java
@@ -11,22 +11,36 @@ import java.util.Date;
  *
  *Lecture on 
  *	inheritance. polymorphism, ...
+-Using super to access parent methods
+-Using parent constructors and constructor chaining
+-Overriding
+-Overriding vs Overloading
+-Object class
+-Polymorphism
+-Dynamic binding
+-instance of
+
  */
 public  class GeometricFigure {
 //private String color="";
 //private boolean filled=false;
 protected  String color="";
 protected boolean filled=false;
+protected String name;
 private Date dateCreated;
 
 	public GeometricFigure(){this.color = "white";
-	this.filled =false;}
- public  GeometricFigure(String color, boolean filled) {
+	this.filled =false; this.name="TBC";}
+ public  GeometricFigure(String name, String color, boolean filled) {
+	this.name= name;
 	this.color = color;
 	this.filled = filled;
 }
 
-public String getColor() {return color;}
+ public String getName() {return this.name;}
+ public void setName(String name) {this.name= name;}
+
+ public String getColor() {return color;}
 public void setColor(String color) {this.color = color;}
 
 public boolean isFilled() {return filled;}
@@ -35,7 +49,7 @@ public void setFilled(boolean filled) {this.filled = filled;}
 public Date getDateCreated() {return dateCreated;}
 public void setDateCreated(Date dateCreated) {this.dateCreated = dateCreated;}
 
-public String toString(){ return "Figure with color:"+this.color +" and created on :"+this.dateCreated;}
+public String toString(){ return "Figure "+this.name+", hascolor:"+this.color +" and created on :"+this.dateCreated;}
 
 }
 //---- define sub class---------------------//
@@ -45,8 +59,8 @@ class Circle extends GeometricFigure
 	
 	//public Circle () {super();}
 	//public Circle (double radius) {this.radius = radius;}
-	public Circle (double radius, String color, boolean filled)
-	{	super(color, filled);
+	public Circle (double radius, String name, String color, boolean filled)
+	{	super(name, color, filled);
 		this.radius = radius;
 	}
 
@@ -68,8 +82,8 @@ class Circle extends GeometricFigure
 	
 	//public Circle () {super();}
 	//public Circle (double radius) {this.radius = radius;}
-	public Rectangle (double width, double height , String color, boolean filled)
-	{	//super(color, filled);
+	public Rectangle (double width, double height , String name, String color, boolean filled)
+	{	super(name, color, filled);
 		this.width= width;
 		this.height= height;		
 	}
@@ -86,3 +100,34 @@ class Circle extends GeometricFigure
 	public String toString () {return super.toString()+". This a Rectangle  with (color, filed, width, height)=("+
 			this.color + ","+ this.filled + "," + this.width + ","+ this.height +")";}
 }
+ //--------------------------
+ //--  testing contsructor chaining
+ 
+ class A {
+	 protected int xA;
+	 //public A() {System.out.println("Setting xA...");this.xA=1;}	 
+	 //public A(int xA) {System.out.println("Setting xA with parame...");this.xA=xA;}	 
+ 
+ }
+ 
+ class B extends A {
+	 protected int xB;
+	 public B() {System.out.println("Setting xB...");this.xB=10;}	 
+	 public B(int xA, int xB) 
+	 	{super();
+	 		System.out.println("Setting xB with parame...");
+	 	this.xB=xB;}	 
+ }
+ class C extends B {
+	 protected int xC;
+	 public C() {System.out.println("Setting xC...");this.xC=100;}	 
+	 public C(int xA, int xB, int xC) 
+	 	{super(xA,xB);
+	 	System.out.println("Setting xA with parame...");
+	 	this.xC=xC;
+	 	}	 
+
+	 public String toString() {return "xA,xB,xC)=("+xA+","+xB+","+xC+")";}
+	 
+ }
+  
